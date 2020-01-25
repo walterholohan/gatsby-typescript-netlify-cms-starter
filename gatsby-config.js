81362e2274677690f1e7e5cc041c7da126656165
Walter Eoin Holohan <walter.holohan@gmail.com>
2020-01-25 14:22:08 +0000

    add offline mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-typescript-netlify-starter`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
@@ -31,6 +31,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
