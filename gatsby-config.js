d6606301d69f1c1fff9942061d99467ab91bacd8
Walter Eoin Holohan <walter.holohan@gmail.com>
2020-01-26 15:37:11 +0000

    rename to add in cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-typescript-netlify-starter`,
+        name: `gatsby-typescript-netlify-cms-starter`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
